0d542a45045066fb79f71651788128f0ffda5b5c
mfsi-akshay <akshaya@mindfiresolutions.com>
2016-05-13 23:21:57 +0530

    Changed State into Props

--- app/assets/javascripts/components/Goal.jsx
+++ app/assets/javascripts/components/Goal.jsx
@@ -1,16 +1,20 @@
+import GoalActions from "../actions/GoalActions.jsx";
+
 export default class Goal extends React.Component{
   constructor(props){
     super(props);
-    this.state = props;
+    //this.state = props;
   }
   complete(goal){
-    this.setState({completed: 'true', end_date: Date.now()});
+    GoalActions.completeGoal(goal);
+    //debugger
+    //this.setState({complete: 'true', end_date: new Date(Date.now())});
   };
   isComplete(goal){
 
   };
   render(){
-    var style = {
+    let style = {
       span: {
         float: 'left',
         margin: '2% 5% 0 3%',
@@ -24,15 +28,17 @@ export default class Goal extends React.Component{
         margin: '5% 0 0 10%'
       }
     };
-    var calc = function(end_date, start_date){
-      var diff = Math.abs(end_date - start_date);
+    let calc = function(end_date, start_date){
+      let sDate = new Date(start_date);
+      let timeDiff = Math.abs(end_date.getTime() - sDate.getTime());
+      let diff = (timeDiff/(60 * 60 * 1000)).toFixed(2);
       return diff;
     }
     return(
         <li className="collection-item">
-          <div className="row" title={this.state.completed ? calc(this.state.end_date, this.state.start_date) : "Created: " + this.props.start_date}>
-            <div className="input-field" ><span style={style.span}>{this.state.body}</span><input disabled={this.state.completed} style={style.input} type="text" placeholder="notes"/>
-              <input type="checkbox" disabled={this.state.completed} checked={this.state.completed} className="filled-in" id={this.props.id} onChange={this.complete.bind(this,this)} /><label htmlFor={this.props.id} style={style.checkbox}></label></div>
+          <div className="row" title={this.props.completed ? calc(this.props.end_date, this.props.start_date) + " hours" : "Created: " + this.props.formattedStartDate}>
+            <div className="input-field" ><span style={style.span}>{this.props.body}</span><input disabled={this.props.complete} style={style.input} type="text" placeholder={this.props.complete ? "complete" : "non complete"} ref="goalNotes"/>
+              <input type="checkbox" disabled={this.props.complete} checked={this.props.complete} className="filled-in" id={this.props.id} onChange={this.complete.bind(this,this)} /><label htmlFor={this.props.id} style={style.checkbox}></label></div>
           </div>
         </li>
     );
